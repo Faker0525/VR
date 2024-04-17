7468d9dd4982047a8f5e5e35642e4d84e17e68ed
Faker0525 <157570288+Faker0525@users.noreply.github.com>
2024-04-17 14:21:29 +0800

    bread

---
+++ bread/data.js
@@ -0,0 +1,254 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "馬可先生外部忠孝店",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.18558133839397328,
+          "pitch": 0.21666908760237114,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": -1.7643602601873987,
+          "pitch": 0.017436607818121175,
+          "rotation": 0.7853981633974483,
+          "target": "2-2"
+        },
+        {
+          "yaw": 1.8245535974797722,
+          "pitch": 0.013452959741698578,
+          "rotation": 5.497787143782138,
+          "target": "4-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.05705192721912766,
+          "pitch": 0.06592047614236485,
+          "title": "馬可先生忠孝店",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "馬可先生內部忠孝店",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.869614950963559,
+          "pitch": 0.666689930614961,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.8906285132151428,
+          "pitch": 0.013329983519673405,
+          "title": "內部環境",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-2",
+      "name": "馬可先生2外部莊敬店",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.306849490817852,
+          "pitch": 0.38429035564719527,
+          "rotation": 0,
+          "target": "3-2"
+        },
+        {
+          "yaw": 1.8182988372439572,
+          "pitch": 0.10468136515659765,
+          "rotation": 0.7853981633974483,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.297974453015083,
+          "pitch": 0.23638995315912226,
+          "title": "莊敬店",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-2",
+      "name": "馬可先生2內部莊敬店",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8811931906279931,
+          "pitch": 0.42378278421213444,
+          "rotation": 0,
+          "target": "2-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.3024910565841026,
+          "pitch": 0.20200973925700794,
+          "title": "內部環境",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "4-3",
+      "name": "馬可先生3內部羅斯福店",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9726992057006072,
+          "pitch": 0.10131738849705663,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.2491076755741943,
+          "pitch": 0.24564795926408856,
+          "title": "羅斯福店內部環境",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
