5f0a01538bcc48a769f8a2ea1514523fd0767914
Faker0525 <157570288+Faker0525@users.noreply.github.com>
2024-04-17 14:28:04 +0800

    up

--- bread/data.js
+++ bread/data.js
@@ -248,7 +248,7 @@ var APP_DATA = {
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
-    "fullscreenButton": false,
+    "fullscreenButton": true,
     "viewControlButtons": true
   }
 };
